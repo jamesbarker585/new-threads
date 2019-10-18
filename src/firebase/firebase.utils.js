808ccf8e60e5d2ad50339651fb1582e5e5df8384
james barker <jamesbarker585@gmail.com>
2019-10-18 15:31:33 -0700

    setup redirect from signin to home

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBTCj62kE7hWMBwc2KO0SzNo_aHpl6foaI",
-  authDomain: "new-threads.firebaseapp.com",
-  databaseURL: "https://new-threads.firebaseio.com",
-  projectId: "new-threads",
-  storageBucket: "new-threads.appspot.com",
-  messagingSenderId: "511913319353",
-  appId: "1:511913319353:web:174b7df7bffec34f11b468",
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
